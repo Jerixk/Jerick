5566272b018457e8de93bfd00154beb04fb0c0a3
Jerixk <152331542+Jerixk@users.noreply.github.com>
2023-11-28 13:23:47 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+class Main{
+  public static void main(String[] args){
+    Spiderman spidy = new Spiderman("Peter Parker 🕷");
+    spidy.setName("Miles Morales");
+    System.out.println("The best spidy is " + spidy.getName() + ".");
+  }
+}
