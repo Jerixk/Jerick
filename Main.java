bbff54d72921b79beb58146b773508015ea6a62d
jacks9n <152330640+jacks9n@users.noreply.github.com>
2023-11-29 13:18:55 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ class Main{
   public static void main(String[] args){
     Spiderman spidy = new Spiderman("Peter Parker 🕷");
     spidy.setName("Miles Morales");
-    System.out.println("The best spidy is " + spidy.getName() + ".");
+    System.out.println("The best spidy is better" + spidy.getName() + ".");
   }
 }
